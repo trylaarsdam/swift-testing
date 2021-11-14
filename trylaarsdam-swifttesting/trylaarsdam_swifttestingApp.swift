b3724fe3627dc6ff3c4f914a068055cdf1e9642e
Todd Rylaarsdam <todd@toddr.org>
2021-11-13 21:46:24 -0600

    updates

---
+++ trylaarsdam-swifttesting/trylaarsdam_swifttestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trylaarsdam_swifttestingApp.swift
+//  trylaarsdam-swifttesting
+//
+//  Created by Todd Rylaarsdam on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct trylaarsdam_swifttestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
