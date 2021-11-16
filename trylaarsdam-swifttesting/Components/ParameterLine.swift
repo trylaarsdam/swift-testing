ae39caad5994ea0a7f9a0f72f557dbcb2b15feaf
Todd Rylaarsdam <todd@toddr.org>
2021-11-16 14:27:20 -0600

    updates

--- trylaarsdam-swifttesting/Components/ParameterLine.swift
+++ trylaarsdam-swifttesting/Components/ParameterLine.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ParameterLine: View {
+    @State var value: Int
+    var min: Int
+    var max: Int
+    var step: Int
+    var name: String
+    
     var body: some View {
         HStack {
-            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: 12) {
-                Text("Transmitter SF")
+            Stepper(value: $value, in: min...max) {
+                Text("\(name)").font(.system(size: 20))
             }
-        }.padding()
+            Text("\(value)").fixedSize(horizontal: true, vertical: false).frame(width: 50, height: 45, alignment: .center).font(.system(size:20)).addBorder(Color.secondary, width: 3, cornerRadius: 10)
+
+        }.padding().addBorder(Color.purple, width: 3, cornerRadius: 10).padding()
     }
 }
 
 struct ParameterLine_Previews: PreviewProvider {
     static var previews: some View {
-        ParameterLine()
+        ParameterLine(value: 8, min: 7, max: 12, step: 1, name: "Transmitter SF")
     }
 }
---
+++ trylaarsdam-swifttesting/Components/RoundedBorder.swift
@@ -0,0 +1,16 @@
+//
+//  RoundedBorder.swift
+//  trylaarsdam-swifttesting
+//
+//  Created by Todd Rylaarsdam on 11/16/21.
+//
+
+import SwiftUI
+
+extension View {
+    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
+        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+        return clipShape(roundedRect)
+             .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+    }
+}
